eb93e3005e298dbc534930bbbb7ae4377f5e789f
Nguyen Thanh Dat <kaanoo2904@gmail.com>
2018-03-30 09:39:42 +0700

    Set layout for frontend

---
+++ frontend/src/components/lessons/Lessons.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'Lessons',
+  data () {
+    return {
+      lessons: [
+        { index: 1, bookName: 'book 1' },
+        { index: 2, bookName: 'book 2' },
+        { index: 3, bookName: 'book 3' }
+      ]
+    }
+  }
+}
+</script>
+
+<template src='./Lessons.html'>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Lessons from '@/components/lessons/Lessons'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    { path: '/', component: Lessons }
   ]
 })
